82925051a0ddff4c78cdbcf08b257524e59e9a4f
Kim Ranyard <kranyard@vmware.com>
2018-12-14 13:32:30 +0000

    Clean up

--- get_endpoints.py
+++ get_endpoints.py
@@ -13,7 +13,6 @@ headers = {'Accept':'application/json;charset=UTF-8','Content-Type':'application
 
 url = "https://{0}/component-registry/endpoints?limit=500&orderby=\"url\"&$filter endpointType/protocol eq \"REST\"".format(host)
 
-#url="https://{0}/catalog-service/api/consumer/requests?limit=30&$orderby=requestNumber%20desc".format(host)
 request = rw.getUrl(url,headers)
 
 print json.dumps(request)
