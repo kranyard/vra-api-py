4c16d62acf870b54edb3cde12ed38182d19405ff
Kim Ranyard <kranyard@vmware.com>
2019-01-13 20:16:34 +0000

    logon

--- logon.py
+++ logon.py
@@ -4,58 +4,85 @@ import os
 import sys
 import json
 import getpass
+
+import argparse
+
 import rw
 
-# Define hostname and credentials
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Display vRA resources/items')
 
-if ( len(sys.argv) > 1 ):
-	if ( sys.argv[1] == "help" ):
-		print ("logon.py <username> <host> <tenant>")
+    parser.add_argument('-s', '--host',
+                        required=False,
+                        action='store',
+                        help='Hostname of vRA server',
+			default = "vra-01a.corp.local")
+
+    parser.add_argument('-u', '--username',
+                        required=False,
+                        action='store',
+                        help='User name',
+			default = "jason@corp.local")
+
+    parser.add_argument('-p', '--password',
+                        required=False,
+                        action='store',
+                        help='Password',
+			default = "VMware1!")
+
+    parser.add_argument('-t', '--tenant',
+                        required=False,
+                        action='store',
+                        help='Tenant',
+			default = "vsphere.local")
+
+    parser.add_argument('-d', '--debug',
+                        action='store_true',
+                        help="Debug, just output JSON",
+                        default=False)
+
+    parser.add_argument('-m', '--metadata',
+                        action='store_true',
+                        help="Display REST metadata",
+                        default=False)
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+
+	args = get_args() 
+
+	# Define hostname and credentials
+
+	print args.username, args.host, args.tenant
+
+	values = { 'username':args.username, 'password':args.password, 'tenant':args.tenant }
+	data = json.dumps(values)
+	headers = {'Accept':'application/json;charset=UTF-8','Content-Type':'application/json;charset=UTF-8'}
+
+	r=rw.postUrl("https://{0}/identity/api/tokens".format(args.host),data=data,headers=headers,showUrl=True)
+
+	resp = r.json()
+
+	if "errors" in resp:
+		print json.dumps(resp)
 		exit(1)
-	else:
-		username=sys.argv[1]
-else:
-	username="jason@corp.local"
 
-if ( len(sys.argv) > 2 ):
-	host=sys.argv[2]
-else:
-	host="vra-01a.corp.local"
+	print "Session started as ["+args.username+"] at ["+args.host+"] and tenant ["+args.tenant+"]"
+	print "Expires at : "+resp["expires"]
+	print "ID Token : ", resp["id"]
 
-if ( len(sys.argv) > 3 ):
-	tenant = sys.argv[3]
-else:
-	tenant="vsphere.local"
+	os.environ['VRATOKEN'] = resp["id"]
+	os.environ['VRATENANT'] = args.tenant
+	os.environ['VRAHOST'] = args.host
+	os.environ['VRAUSER'] = args.username
+	os.environ['VRAEXPIRY'] = resp["expires"]
+	os.system("/bin/bash -i") 
 
-if ( len(sys.argv) > 4 ):
-	if ( sys.argv[4] == "prompt" ):
-		password=getpass.getpass()
-	else:
-		password = sys.argv[4]
-else:
-	password="VMware1!"
-
-print username, host, tenant
-
-values = { 'username':username, 'password':password, 'tenant':tenant }
-data = json.dumps(values)
-headers = {'Accept':'application/json;charset=UTF-8','Content-Type':'application/json;charset=UTF-8'}
-
-r=rw.postUrl("https://{0}/identity/api/tokens".format(host),data=data,headers=headers,showUrl=True)
-
-resp = r.json()
-
-if "errors" in resp:
-	print json.dumps(resp)
-	exit(1)
-
-print "Session started as ["+username+"] at ["+host+"] and tenant ["+tenant+"]"
-print "Expires at : "+resp["expires"]
-print "ID Token : ", resp["id"]
-
-os.environ['VRATOKEN'] = resp["id"]
-os.environ['VRATENANT'] = tenant
-os.environ['VRAHOST'] = host
-os.environ['VRAUSER'] = username
-os.environ['VRAEXPIRY'] = resp["expires"]
-os.system("/bin/bash -i") 
+# Start program
+if __name__ == "__main__":
+    main()
